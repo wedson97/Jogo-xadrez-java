447ba6329ed39378fc48285ea6e1f60ef79c76e7
Wedson Silva <wedsonsilva025@gmail.com>
2023-06-05 21:02:01 -0300

    Implementado movimentos do rei

--- src/main/java/Xadrez/Rei.java
+++ src/main/java/Xadrez/Rei.java
@@ -1,6 +1,7 @@
 package Xadrez;
 
 import BoardGame.Board;
+import BoardGame.Posicao;
 
 public class Rei extends PecaDeXadrez{
     
@@ -12,9 +13,56 @@ public class Rei extends PecaDeXadrez{
         return "R";
     }
 
+    public boolean podeMover(Posicao posicao){
+        PecaDeXadrez p = (PecaDeXadrez)getBoard().pecas(posicao);
+        return p==null || p.getCor()!= getCor();
+    }
+    
     @Override
     public boolean[][] possivelMovimentos() {
         boolean[][] matriz = new boolean[getBoard().getLinhas()][getBoard().getColunas()];
+        Posicao p = new Posicao(0,0);
+        //cima
+        p.setValor(position.getLinha()-1, position.getColuna());
+        if(getBoard().posicaoExistente(p) && podeMover(p)){
+            matriz[p.getLinha()][p.getColuna()]=true;
+        }
+        //baixo
+        p.setValor(position.getLinha()+1, position.getColuna());
+        if(getBoard().posicaoExistente(p) && podeMover(p)){
+            matriz[p.getLinha()][p.getColuna()]=true;
+        }
+        //esquerda
+        p.setValor(position.getLinha(), position.getColuna()-1);
+        if(getBoard().posicaoExistente(p) && podeMover(p)){
+            matriz[p.getLinha()][p.getColuna()]=true;
+        }
+        //direita
+        p.setValor(position.getLinha(), position.getColuna()+1);
+        if(getBoard().posicaoExistente(p) && podeMover(p)){
+            matriz[p.getLinha()][p.getColuna()]=true;
+        }
+        //nw
+        p.setValor(position.getLinha()-1, position.getColuna()-1);
+        if(getBoard().posicaoExistente(p) && podeMover(p)){
+            matriz[p.getLinha()][p.getColuna()]=true;
+        }
+        //nordeste
+        p.setValor(position.getLinha()-1, position.getColuna()+1);
+        if(getBoard().posicaoExistente(p) && podeMover(p)){
+            matriz[p.getLinha()][p.getColuna()]=true;
+        }
+        //sulOeste
+        p.setValor(position.getLinha()+1, position.getColuna()-1);
+        if(getBoard().posicaoExistente(p) && podeMover(p)){
+            matriz[p.getLinha()][p.getColuna()]=true;
+        }
+        //sulDeste
+        p.setValor(position.getLinha()+1, position.getColuna()+1);
+        if(getBoard().posicaoExistente(p) && podeMover(p)){
+            matriz[p.getLinha()][p.getColuna()]=true;
+        }
+        
         return matriz;
     }
 }
